10f17584211eedde0e5169ff5d22daf91de037e4
Tristan Thorp <104901958@student.swin.edu.au>
2024-05-09 15:20:55 +1000

    added everyone to htaccess file, also fixed a couple typos

--- Database/StoredProcedures/sp_getApplicationsForLIsting.sql
+++ Database/StoredProcedures/sp_getApplicationsForLIsting.sql
@@ -1,6 +1,6 @@
-DROP PROCEDURE IF EXISTS sp_getApplicationsForLIsting;
+DROP PROCEDURE IF EXISTS sp_getApplicationsForListing;
 DELIMITER //
-CREATE PROCEDURE sp_getApplicationsForLIsting()
+CREATE PROCEDURE sp_getApplicationsForListing()
 BEGIN
     SELECT Id, name FROM JobSkills;
 END //
